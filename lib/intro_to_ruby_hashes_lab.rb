1743044461da24df369edeb6e1a975d4d8128ef2
Jacqueline <lojacqueline2@gmail.com>
2020-09-02 05:28:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -18,5 +18,6 @@ else
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+   hash[key
 end
 end
