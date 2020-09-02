44e4e7247fb59882d4465d16826b74a5844b15e3
Jacqueline <lojacqueline2@gmail.com>
2020-09-02 05:21:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,9 +5,9 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
- {:hash => :key}
+hash[key]
 
-read_from_hash[hash]
+
 end
 
 def update_counting_hash(hash, key)
